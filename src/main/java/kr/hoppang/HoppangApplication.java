5f3c2490c96ddb5d5a935b2958c832f8d37c48cc
taewoongjung <taewj@howser.co.kr>
2024-08-17 18:55:13 +0900

    프로젝트 생성

---
+++ src/main/java/kr/hoppang/HoppangApplication.java
@@ -0,0 +1,13 @@
+package kr.hoppang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/hoppang/HoppangApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hoppang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
