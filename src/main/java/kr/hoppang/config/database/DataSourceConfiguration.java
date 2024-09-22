a6eea0581a2f85a10eaf915c86bca53c96487016
taewoongjung <taewj@howser.co.kr>
2024-09-22 12:42:14 +0900

    master-slave 구조로 변경

---
+++ src/main/java/kr/hoppang/config/database/DataSourceConfiguration.java
@@ -0,0 +1,44 @@
+package kr.hoppang.config.database;
+
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableTransactionManagement
+public class DataSourceConfiguration {
+
+    @Bean(name = "writeDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.master")
+    public DataSource writeDataSource() {
+        return new HikariDataSource();
+    }
+
+    @Bean(name = "readOnlyDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.slave")
+    public DataSource readOnlyDataSource() {
+        return new HikariDataSource();
+    }
+
+    @Bean(name = "routingDataSource")
+    public DataSource routingDataSource(
+        @Qualifier("writeDataSource") DataSource writeDataSource,
+        @Qualifier("readOnlyDataSource") DataSource readOnlyDataSource) {
+        return new WriteOrReadOnlyRoutingDataSource(writeDataSource, readOnlyDataSource);
+    }
+
+    @Primary
+    @Bean(name = "dataSource")
+    public LazyConnectionDataSourceProxy dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+}
---
+++ src/main/java/kr/hoppang/config/database/RoutingType.java
@@ -0,0 +1,6 @@
+package kr.hoppang.config.database;
+
+public enum RoutingType {
+    WRITE,
+    READ_ONLY
+}
