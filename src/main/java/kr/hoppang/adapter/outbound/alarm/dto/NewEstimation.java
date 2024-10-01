37c094a73cbebb0d80577c268a08fa77f7cb357a
taewoongjung <taewj@howser.co.kr>
2024-10-01 11:17:02 +0900

    slack 알람 회사 정보 추가

--- src/main/java/kr/hoppang/adapter/outbound/alarm/dto/NewEstimation.java
+++ src/main/java/kr/hoppang/adapter/outbound/alarm/dto/NewEstimation.java
@@ -4,24 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 import kr.hoppang.application.command.chassis.commands.CalculateChassisPriceCommand.CalculateChassisPrice;
 import kr.hoppang.domain.chassis.ChassisType;
+import kr.hoppang.domain.chassis.CompanyType;
 
 public record NewEstimation(String userName,
                             String userAddress,
-                            String company,
+                            CompanyType company,
                             List<Estimation> estimationList) {
 
     public static NewEstimation of(
             final String userName,
             final String userAddress,
-            final String company,
+            final CompanyType company,
             final List<CalculateChassisPrice> calculateChassisList) {
 
         List<Estimation> estimationList = new ArrayList<>();
 
-        calculateChassisList.forEach(e -> estimationList.add(new Estimation(
-                e.chassisType(),
-                e.width(), e.height()
-        )));
+        calculateChassisList.forEach(e -> estimationList.add(
+                new Estimation(
+                        e.chassisType(),
+                        e.width(), e.height()
+                )));
 
         return new NewEstimation(userName, userAddress, company, estimationList);
     }
