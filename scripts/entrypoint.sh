90c30267fd453c02a64a039671ca00b8c726f498
taewoongjung <taewj@howser.co.kr>
2024-10-13 02:20:57 +0900

    프로메테우스 그라파나 컨테이너 생성

--- scripts/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export SERVER_ID=$(hostname)
-exec java -jar -Djava.security.egd=file:/dev/./urandom \
-     -Dsun.net.inetaddr.ttl=0 \
-     -Dspring.profiles.active=prod \
-     app.jar
