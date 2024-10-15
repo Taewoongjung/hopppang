3a57b74caeba1c12cd0236ba99d89d7aa5aed96b
taewoongjung <taewj@howser.co.kr>
2024-10-15 16:46:39 +0900

    임시로 테스트 키 적용

--- src/main/java/kr/hoppang/adapter/common/exception/custom/HoppangDuplicatedLoginException.java
+++ src/main/java/kr/hoppang/adapter/common/exception/custom/HoppangDuplicatedLoginException.java
@@ -11,7 +11,7 @@ public class HoppangDuplicatedLoginException extends RuntimeException {
     private final String oauthType;
 
     public HoppangDuplicatedLoginException(final String email, final OauthType oauthType) {
-        super("기존 호빵 계정에 연동된 소셜 계정이 있습니다.\n해당 소셜로 로그인 해주세요.");
+        super("기존 호빵 계정에 연동된 소셜 계정이 있습니다. 해당 소셜로 로그인 해주세요.");
 
         this.code = 1;
         this.email = email;
