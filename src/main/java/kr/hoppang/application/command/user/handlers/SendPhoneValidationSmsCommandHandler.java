37330d2b97924f371b9e25ed922c7ef661e02b99
taewoongjung <taewj@howser.co.kr>
2024-11-10 22:06:23 +0900

    CLN: 회원이 중복 되면 캐시에서도 회원 정보 제거

--- src/main/java/kr/hoppang/application/command/user/handlers/SendPhoneValidationSmsCommandHandler.java
+++ src/main/java/kr/hoppang/application/command/user/handlers/SendPhoneValidationSmsCommandHandler.java
@@ -5,12 +5,15 @@ import static kr.hoppang.adapter.common.util.CheckUtil.duplicatedSsoLoginCheck;
 import kr.hoppang.abstraction.domain.ICommandHandler;
 import kr.hoppang.abstraction.serviceutill.IThirdPartyValidationCheckSender;
 import kr.hoppang.adapter.inbound.user.webdto.ValidationType;
+import kr.hoppang.adapter.outbound.cache.dto.TearDownBucketByKey;
 import kr.hoppang.application.command.user.commands.SendPhoneValidationSmsCommand;
 import kr.hoppang.domain.user.OauthType;
 import kr.hoppang.domain.user.User;
 import kr.hoppang.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,15 +22,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class SendPhoneValidationSmsCommandHandler implements
         ICommandHandler<SendPhoneValidationSmsCommand, Void> {
 
-    private final IThirdPartyValidationCheckSender smsThirdPartyValidationCheckSender;
     private final UserRepository userRepository;
+    private final ApplicationEventPublisher eventPublisher;
+    private final IThirdPartyValidationCheckSender smsThirdPartyValidationCheckSender;
 
     public SendPhoneValidationSmsCommandHandler(
-            @Qualifier("SmsUtilService") final IThirdPartyValidationCheckSender smsUtilService,
-            final UserRepository userRepository
+            final UserRepository userRepository,
+            final ApplicationEventPublisher eventPublisher,
+            @Qualifier("SmsUtilService") final IThirdPartyValidationCheckSender smsUtilService
     ) {
-        this.smsThirdPartyValidationCheckSender = smsUtilService;
         this.userRepository = userRepository;
+        this.eventPublisher = eventPublisher;
+        this.smsThirdPartyValidationCheckSender = smsUtilService;
     }
 
     @Override
@@ -44,9 +50,11 @@ public class SendPhoneValidationSmsCommandHandler implements
             // 존재하는 휴대폰 번호 인지 검증
             User user = userRepository.findByEmail(event.email());
             if (user != null && !user.getEmail().equals(event.email())) {
-                // 이미 회원이 존재하면 갓 만들어진 회원 제거
+                // 이미 회원이 존재하면 갓 만들어진 회원 제거 start
                 userRepository.deleteUser(event.email());
+                eventPublisher.publishEvent(new TearDownBucketByKey("users::" + event.email()));
                 log.info("중복으로 인한 유저 제거 완료 = {}", event.email());
+                // 이미 회원이 존재하면 갓 만들어진 회원 제거 end
 
                 // 이미 회원이 존재하면 에러를 던진다.
                 duplicatedSsoLoginCheck(!OauthType.NON.equals(user.getOauthType()), user.getEmail(),
