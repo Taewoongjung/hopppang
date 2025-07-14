a940f6350ba562eeda876b2be0d7f45c88eeae04
taewoongjung <taewj@howser.co.kr>
2025-07-14 10:24:49 +0900

    CLN: 좋아요 로직 수정 - 4

--- src/main/java/kr/hoppang/application/command/boards/event/hanlders/RemovePostsLikeCommandEventHandler.java
+++ src/main/java/kr/hoppang/application/command/boards/event/hanlders/RemovePostsLikeCommandEventHandler.java
@@ -1,7 +1,12 @@
 package kr.hoppang.application.command.boards.event.hanlders;
 
+import jakarta.annotation.PostConstruct;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.stream.Collectors;
 import kr.hoppang.application.command.boards.event.events.RemovePostsLikeCommandEvent;
 import kr.hoppang.domain.boards.PostsLike;
+import kr.hoppang.domain.boards.repository.BoardsRepositoryStrategy;
 import kr.hoppang.domain.boards.repository.PostsLikeCommandRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
@@ -12,18 +17,41 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class RemovePostsLikeCommandEventHandler {
 
-    private final PostsLikeCommandRepository postsLikeCommandRepository;
+    private final List<PostsLikeCommandRepository> postsLikeCommandRepositoryList;
+    private EnumMap<BoardsRepositoryStrategy, PostsLikeCommandRepository> postsLikeCommandRepositoryEnumMap;
+
+    @PostConstruct
+    void init() {
+        postsLikeCommandRepositoryEnumMap = postsLikeCommandRepositoryList.stream()
+                .collect(Collectors.toMap(
+                        PostsLikeCommandRepository::strategy,
+                        (postsLikeCommandRepository) -> postsLikeCommandRepository,
+                        (existRepository, newRepository) -> existRepository,
+                        () -> new EnumMap<>(BoardsRepositoryStrategy.class)
+                ));
+    }
 
 
     @Async
     @EventListener
     public void handle(final RemovePostsLikeCommandEvent command) {
 
-        postsLikeCommandRepository.delete(
-                PostsLike.builder()
-                        .postId(command.postId())
-                        .userId(command.unlikedUserId())
-                        .build()
-        );
+        try {
+
+            postsLikeCommandRepositoryEnumMap.get(BoardsRepositoryStrategy.CACHE).delete(
+                    PostsLike.builder()
+                            .postId(command.postId())
+                            .userId(command.unlikedUserId())
+                            .build()
+            );
+
+            postsLikeCommandRepositoryEnumMap.get(BoardsRepositoryStrategy.RDB).delete(
+                    PostsLike.builder()
+                            .postId(command.postId())
+                            .userId(command.unlikedUserId())
+                            .build()
+            );
+
+        } catch (Exception ignored) {}
     }
 }
