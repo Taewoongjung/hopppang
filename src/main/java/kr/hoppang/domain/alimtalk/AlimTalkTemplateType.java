b80b4eeef1c360ce26c2dd39e9db0d20aa82e8c9
taewoongjung <taewj@howser.co.kr>
2025-08-06 16:37:14 +0900

    CLN: 회원가입 알림톡 템플릿 변경

--- src/main/java/kr/hoppang/domain/alimtalk/AlimTalkTemplateType.java
+++ src/main/java/kr/hoppang/domain/alimtalk/AlimTalkTemplateType.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AlimTalkTemplateType {
-    COMPLETE_SIGNUP("회원가입 완료", "UB_4026"),
+    COMPLETE_SIGNUP("회원가입 완료", "UB_4886"),
     WINDOW_INQUIRY_REPLY("담당직원 연락 필요 여부 요청", "TY_9241");
 
     private final String description;
